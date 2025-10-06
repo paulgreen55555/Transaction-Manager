223e01ea4f1e8f7316afbd8de27e5497ed4512e9
Pavel <paulgreen55555@gmail.com>
2025-10-06 18:20:26 -0400

    Update context model and add migration

--- TransactionManager.Api/Data/Migrations/20251006221001_AddConstraints.Designer.cs
+++ TransactionManager.Api/Data/Migrations/20251006221001_AddConstraints.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TransactionManager.Api.Data;
 
@@ -10,9 +11,11 @@ using TransactionManager.Api.Data;
 namespace TransactionManager.Api.Data.Migrations
 {
     [DbContext(typeof(TransactionMangerContext))]
-    partial class TransactionMangerContextModelSnapshot : ModelSnapshot
+    [Migration("20251006221001_AddConstraints")]
+    partial class AddConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
--- TransactionManager.Api/Entities/Transaction.cs
+++ TransactionManager.Api/Entities/Transaction.cs
@@ -1,9 +1,12 @@
-﻿namespace TransactionManager.Api.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionManager.Api.Entities
 {
     public class Transaction
     {
         public Guid Id { get; set; }
 
+        [MaxLength(50)]
         public string? Description { get; set; }
 
         public decimal Amount { get; set; }
